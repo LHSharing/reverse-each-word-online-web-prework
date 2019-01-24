dc576021ac9638c9f24bcfef25de0907abe3eb09
Lu Daley <daleyluis@gmail.com>
2019-01-24 15:51:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
    array = sentence.split
-   new_array = array.each {|n| n.reverse}
+   new_array = array.collect {|n| n.reverse}
    new_array.join(" ")
  end
    
